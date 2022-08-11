a19f704ebd8c6d091914d0f0ed2b31cc219d0d3e
Gabriel Mendes <gabrielsmm251@gmail.com>
2022-08-11 20:18:24 -0300

    Projeto criado

---
+++ backend/src/main/java/com/gabriel/sga/SgaApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.sga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgaApplication.class, args);
+	}
+
+}
