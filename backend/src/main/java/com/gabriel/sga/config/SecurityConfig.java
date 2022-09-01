932ba382f079d4290aaa3b6e09f074957119bec7
Gabriel Mendes <gabrielsmm251@gmail.com>
2022-09-01 20:26:59 -0300

    Config CORS deploy

--- backend/src/main/java/com/gabriel/sga/config/SecurityConfig.java
+++ backend/src/main/java/com/gabriel/sga/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://coletaconsciente.netlify.app"));
 		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
